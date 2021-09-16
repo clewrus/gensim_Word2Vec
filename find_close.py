bab5767e77ea31f7e9c41c2b3886f878bdfb7196
clewrus <clewrus@gmail.com>
2021-09-16 09:59:14 +0300

    Added comments

--- find_close.py
+++ find_close.py
@@ -2,8 +2,18 @@ from gensim.models import Word2Vec
 import pymorphy2
 import sys
 
+dataset_name = sys.argv[1]
+target_word = sys.argv[2]
+
+# getting normal form of the word
 morph = pymorphy2.MorphAnalyzer( lang='uk' )
-model = Word2Vec.load( sys.argv[1] )
-normal_word = morph.parse( sys.argv[2] )[0].normal_form
+normal_word = morph.parse( target_word )[0].normal_form
+
+# loading model
+model = Word2Vec.load( dataset_name )
+
+# getting most similar words
 close_words = model.wv.most_similar( normal_word )
+
+print( target_word )
 print( '\n'.join( map(str, close_words) ) )
